c6d03f60ef6386564a3bd7c55b5cd5f560b01f45
Maya <maya.noorallah.89@gmail.com>
2020-02-21 17:55:41 +0300

    add getCryptoCurrencyInformation function to pricesController

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Bitcoin Price tracker API</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -59,12 +59,12 @@
             }
 
             .m-b-md {
-                margin-bottom: 30px;
+                margin: auto auto;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        {{-- <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -77,7 +77,7 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            @endif --}}
 
             <div class="content">
                 <div class="title m-b-md">
